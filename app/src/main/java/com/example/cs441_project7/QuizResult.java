76e6fae5d2b15a264847d4a8bf05be59f97beef4
aliang30 <aliang30@binghamton>
2019-12-07 21:36:37 -0500

    Added quizResult activity

---
+++ app/src/main/java/com/example/cs441_project7/QuizResult.java
@@ -0,0 +1,17 @@
+package com.example.cs441_project7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class QuizResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_result);
+
+
+    }
+}
